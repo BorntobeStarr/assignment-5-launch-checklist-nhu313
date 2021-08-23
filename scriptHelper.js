b6b0e232dd8543f79cb45f5357b2fc6e41a97ade
Starr <starrleer@gmail.com>
2021-08-23 18:51:15 -0400

    formSubmission transfer complete

--- scriptHelper.js
+++ scriptHelper.js
@@ -49,28 +49,59 @@ if (
   } else if (isNaN(fuelLevelValue.value) || isNaN(cargoLevelValue.value)){
     alert("User must enter valid input");
   } 
-  // set the list.style.visibility = 'visible'
+        // set the list.style.visibility = 'visible'
     else {
       // get the pilot status, update the inner HTML to say `Pilot ${pilotValue} is ready for launch`
       document.getElementById("pilotStatus").style.visibility = 'visible';
-      document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotValue} is ready for launch`
-
-// get the copilot status, update the inner HTML to say `CoPilot ${copilotValue} is ready for launch`
+      document.getElementById("pilotStatus").innerHTML = `Pilot ${pilotValue} is ready for launch`;
+    // get the copilot status, update the inner HTML to say `CoPilot ${copilotValue} is ready for launch`
       document.getElementById("copilotStatus").style.visibility = 'visible';
-      document.getElementById("copilotStatus").innerHTML = `CoPilot ${copilotValue} is ready for launch`
+      document.getElementById("copilotStatus").innerHTML = `CoPilot ${copilotValue} is ready for launch`;
     }
 
   // check if the fuel level is less 10,000
     // change launchStatus to "Shuttle not ready for launch", and color to red
     // change the fuelStatus to "Fuel level too low for launch"
+  let lowFuelLevel = fuelLevelValue.value <10000;
+    if (lowFuelLevel === true){
+      document.getElementById("fuelStatus").style.visibility = "visable";
+      document.getElementById("fuelStatus").innerHTML = 
+          "Fuel level too low for launch";
+      document.getElementById("fuelStatus").style.color = "red";
+      document.querySelector("h2").style.color = "red";
+    }
 
 
   // check if the cargo level is more than 10,000
     // change launchStatus to "Shuttle not ready for launch", and color to red
     // change the cargoStatus to "Cargo level too high for launch"
+  let highCargoLevel = cargoLevelValue.value > 10000; 
+    if (highCargoLevel){
+      document.getElementById("cargoStatus").style.visibility = "visable";
+      document.getElementById("cargoStatus").innerHTML = 
+        "Cargo level too high for launch";
+      document.getElementById("cargoStatus").style.color = "red";
+      document.querySelector("h2").style.color = "red";
+    }
 
     // if both fuel and cargo are good
       // change the launchStatus to "Shuttle is Ready for Launch" and color to green
+  if (!lowFuelLevel && !highCargoLevel){
+    document.getElementById("launchStatus").innerHTML = 
+      "Shuttle is Ready for Launch";
+    document.getElementById("launchStatus").style.color = green
+
+    //Fuel level pass code:
+    document.getElementById("fuelStatus").innerHTML = 
+    "Fuel level has passed";
+    document.getElementById("fuelStatus").style.color = "green";
+    //CARGO MASS PASS CODE:
+    document.getElementById("cargoStatus").innerHTML = 
+    "Cargo level has passed";
+  document.getElementById("cargoStatus").style.color = "green";
+  }
+
+
 }
 
 async function myFetch() {
